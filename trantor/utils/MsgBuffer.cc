0b5a54da1a202f532c81d3a8e39779525d36e27e
an-tao <20741618@qq.com>
2017-01-31 23:13:24 +0800

    modified class MsgBuffer

--- trantor/utils/MsgBuffer.cc
+++ trantor/utils/MsgBuffer.cc
@@ -1,7 +1,11 @@
 #include <trantor/utils/MsgBuffer.h>
+#include <trantor/utils/Funcs.h>
 #include <string.h>
 #include <sys/uio.h>
 #include <errno.h>
+#include <netinet/in.h>
+#include <assert.h>
+
 using namespace trantor;
 #define BUF_OFFSET 8
 MsgBuffer::MsgBuffer(size_t len)
@@ -22,7 +26,12 @@ void MsgBuffer::ensureWritableBytes(size_t len)
         return;
     }
     //create new buffer
-    MsgBuffer newbuffer(BUF_OFFSET+readableBytes()+len);
+    size_t newLen;
+    if((buffer_.size()*2)>(BUF_OFFSET+readableBytes()+len))
+        newLen=buffer_.size()*2;
+    else
+        newLen=BUF_OFFSET+readableBytes()+len;
+    MsgBuffer newbuffer(newLen);
     newbuffer.append(*this);
     swap(newbuffer);
 }
@@ -41,6 +50,41 @@ void MsgBuffer::append(const char *buf, size_t len) {
     memcpy(&buffer_[tail_],buf,len);
     tail_ +=len;
 }
+void MsgBuffer::appendInt16(const uint16_t s)
+{
+    uint16_t ss=htons(s);
+    append(static_cast<const char*>((void *)&ss),2);
+}
+void MsgBuffer::appendInt32(const uint32_t i)
+{
+    uint32_t ii=htonl(i);
+    append(static_cast<const char*>((void *)&ii),4);
+}
+void MsgBuffer::appendInt64(const uint64_t l)
+{
+    uint64_t ll=hton64(l);
+    append(static_cast<const char*>((void *)&ll),8);
+}
+
+const uint16_t MsgBuffer::peekInt16() const
+{
+    assert(readableBytes()>=2);
+    uint16_t rs=*(static_cast<const uint16_t *>((void *)peek()));
+    return ntohs(rs);
+}
+const uint32_t MsgBuffer::peekInt32() const
+{
+    assert(readableBytes()>=4);
+    uint32_t rl=*(static_cast<const uint32_t *>((void *)peek()));
+    return ntohl(rl);
+}
+const uint64_t MsgBuffer::peekInt64() const
+{
+    assert(readableBytes()>=8);
+    uint64_t rll=*(static_cast<const uint64_t *>((void *)peek()));
+    return ntoh64(rll);
+}
+
 void MsgBuffer::retrieve(size_t len) {
     if(len>=readableBytes())
     {
@@ -75,4 +119,56 @@ size_t MsgBuffer::readFd(int fd,int *retErrno){
         append(extBuffer,n-writable);
     }
     return n;
+}
+
+std::string MsgBuffer::read(uint64_t len)
+{
+    if(len>readableBytes())
+        len=readableBytes();
+    std::string ret(peek(),len);
+    retrieve(len);
+    return ret;
+}
+uint8_t MsgBuffer::readInt8()
+{
+    uint8_t ret=peekInt8();
+    retrieve(1);
+    return ret;
+}
+uint16_t MsgBuffer::readInt16()
+{
+    uint16_t ret=peekInt16();
+    retrieve(2);
+    return ret;
+}
+uint32_t MsgBuffer::readInt32()
+{
+    uint32_t ret=peekInt32();
+    retrieve(4);
+    return ret;
+}
+uint64_t MsgBuffer::readInt64()
+{
+    uint64_t ret=peekInt64();
+    retrieve(8);
+    return ret;
+}
+
+void MsgBuffer::addInFront(const char *buf,size_t len)
+{
+    if(head_>=len)
+    {
+        memcpy(begin()+head_-len,buf,len);
+        head_ -= len;
+        return;
+    }
+    size_t newLen;
+    if(len+readableBytes()<BUFFER_DEF_LENGTH)
+        newLen=BUFFER_DEF_LENGTH;
+    else
+        newLen=len+readableBytes();
+    MsgBuffer newBuf(newLen);
+    newBuf.append(buf,len);
+    newBuf.append(*this);
+    swap(newBuf);
 }
--- trantor/utils/MsgBuffer.h
+++ trantor/utils/MsgBuffer.h
@@ -4,23 +4,44 @@
 #include <vector>
 #include <stdio.h>
 #include <string>
+#include <assert.h>
+#define BUFFER_DEF_LENGTH 2048
 namespace trantor
 {
     class MsgBuffer
     {
     public:
-        MsgBuffer(size_t len=2048);
+        MsgBuffer(size_t len=BUFFER_DEF_LENGTH);
+        //peek
         const char *peek() const {return begin()+head_;}
-
+        const uint8_t peekInt8() const {
+            assert(readableBytes()>=1);
+            return *(static_cast<const uint8_t*>((void *)peek()));}
+        const uint16_t peekInt16() const;
+        const uint32_t peekInt32() const;
+        const uint64_t peekInt64() const;
+        //read
+        std::string read(uint64_t len);
+        uint8_t readInt8();
+        uint16_t readInt16();
+        uint32_t readInt32();
+        uint64_t readInt64();
         void swap(MsgBuffer &buf);
         const size_t readableBytes() const {return tail_-head_;}
         const size_t writableBytes() const {return buffer_.size()-tail_;}
-
+//append
         void append(const MsgBuffer &buf);
         void append(const char *buf,size_t len);
         void append(const std::string &buf){
             ensureWritableBytes(buf.length());
             append(buf.c_str(),buf.length());}
+        void appendInt8(const uint8_t b){append(static_cast<const char*>((void *)&b),1);}
+        void appendInt16(const uint16_t s);
+        void appendInt32(const uint32_t i);
+        void appendInt64(const uint64_t l);
+//add in front
+        void addInFront(const char *buf,size_t len);
+
         void retrieveAll();
         void retrieve(size_t len);
         size_t readFd(int fd,int *retErrno);
