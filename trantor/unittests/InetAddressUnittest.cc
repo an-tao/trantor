ca57a4d20a1d09ab20cb02ea0bfef5dca90379df
antao <antao@guoxin.cn>
2018-08-31 16:21:36 +0800

    Add a unittest

---
+++ trantor/unittests/InetAddressUnittest.cc
@@ -0,0 +1,19 @@
+#include <trantor/net/InetAddress.h>
+#include <gtest/gtest.h>
+#include <string>
+#include <iostream>
+using namespace trantor;
+TEST(InetAddress, innerIpTest)
+{
+    EXPECT_EQ(true,InetAddress("192.168.0.1",0).isInnerIp());
+EXPECT_EQ(true,InetAddress("192.168.12.1",0).isInnerIp());
+EXPECT_EQ(true,InetAddress("10.168.0.1",0).isInnerIp());
+EXPECT_EQ(true,InetAddress("10.0.0.1",0).isInnerIp());
+EXPECT_EQ(true,InetAddress("172.31.10.1",0).isInnerIp());
+EXPECT_EQ(true,InetAddress("127.0.0.1",0).isInnerIp());
+}
+int main( int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
