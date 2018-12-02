bb4a6d01c72450c8596ff3763e4c24c60755d1f4
antao <antao2002@gmail.com>
2018-12-02 22:39:12 +0800

    Add the toDbStringLocal() method to the Date class

--- trantor/utils/Date.h
+++ trantor/utils/Date.h
@@ -41,11 +41,38 @@ class Date
     }
     int64_t microSecondsSinceEpoch() const { return microSecondsSinceEpoch_; }
     struct tm tmStruct() const;
-    std::string toFormattedString(bool showMicroseconds) const;                                            //UTC
-    std::string toCustomedFormattedString(const std::string &fmtStr, bool showMicroseconds = false) const; //UTC
 
-    std::string toFormattedStringLocal(bool showMicroseconds) const;                                            //Local time zone
-    std::string toCustomedFormattedStringLocal(const std::string &fmtStr, bool showMicroseconds = false) const; //Local time zone
+    /// Generate a UTC time string
+    /**
+     * Example:
+     * 20180101 10:10:25            //If the @param showMicroseconds is false
+     * 20180101 10:10:25:102414     //If the @param showMicroseconds is true
+     */
+    std::string toFormattedString(bool showMicroseconds) const;
+    
+    /// Generate a UTC time string formated by the @param fmtStr
+    /**
+     * The @param fmtStr is the format string for the function strftime()
+     * Example:
+     * 2018-01-01 10:10:25          //If the @param fmtStr is "%Y-%m-%d %H:%M:%S" and the @param showMicroseconds is false
+     * 2018-01-01 10:10:25:102414   //If the @param fmtStr is "%Y-%m-%d %H:%M:%S" and the @param showMicroseconds is true
+     */
+    std::string toCustomedFormattedString(const std::string &fmtStr, bool showMicroseconds = false) const;
+
+    /// Generate a local time zone string, the format of the string is same as the mothed toFormattedString
+    std::string toFormattedStringLocal(bool showMicroseconds) const;
+
+    /// Generate a local time zone string formated by the @param fmtStr
+    std::string toCustomedFormattedStringLocal(const std::string &fmtStr, bool showMicroseconds = false) const;
+
+    /// Generate a local time zone string for database;
+    /**
+     * Example:
+     * 2018-01-01                   //If hours, minutes, seconds and microseconds are zero
+     * 2018-01-01 10:10:25          //If the microsecond is zero
+     * 2018-01-01 10:10:25:102414   //If the microsecond is not zero
+     */
+    std::string toDbStringLocal() const;
 
     void toCustomedFormattedString(const std::string &fmtStr, char *str, size_t len) const; //UTC
     bool isSameSecond(const Date &date) const
