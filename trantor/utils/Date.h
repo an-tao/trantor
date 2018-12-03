ad4c6935d90ffa5fd9865e5b0876955e15dd3b90
antao <antao2002@gmail.com>
2018-12-03 14:17:55 +0800

    Add some comments to the Date class

--- trantor/utils/Date.h
+++ trantor/utils/Date.h
@@ -11,10 +11,37 @@ class Date
 {
   public:
     Date() : microSecondsSinceEpoch_(0){};
-    ~Date(){};
-    const Date after(double second) const;
+    explicit Date(int64_t microSec) : microSecondsSinceEpoch_(microSec){};
+    Date(unsigned int year,
+         unsigned int month,
+         unsigned int day,
+         unsigned int hour = 0,
+         unsigned int minute = 0,
+         unsigned int second = 0,
+         unsigned int microSecond = 0);
+
+    /// Create a Date object that represents the current time;
     static const Date date();
+
+    /// Same as method date()
     static const Date now() { return Date::date(); }
+
+    /// Create a Date object that represents the time after '@param second' seconds from *this
+    /**
+     * Note:
+     * The @param second can be negative to indicate an earlier time than *this
+     */
+    const Date after(double second) const;
+
+    /// Create a Date object equal to * this, but the number of microseconds is zero.
+    const Date roundSecond() const;
+    
+    /// Create a Date object equal to * this, but numbers of hours, minutes, seconds and microseconds are zero.
+    const Date roundDay() const;
+
+    ~Date(){};
+    
+    
     bool operator==(const Date &date) const
     {
         return microSecondsSinceEpoch_ == date.microSecondsSinceEpoch_;
@@ -75,25 +102,16 @@ class Date
     std::string toDbStringLocal() const;
 
     void toCustomedFormattedString(const std::string &fmtStr, char *str, size_t len) const; //UTC
+
     bool isSameSecond(const Date &date) const
     {
         return microSecondsSinceEpoch_ / MICRO_SECONDS_PRE_SEC == date.microSecondsSinceEpoch_ / MICRO_SECONDS_PRE_SEC;
     }
-    const Date roundSecond() const;
-    const Date roundDay() const;
+
     void swap(Date &that)
     {
         std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
     }
-
-    explicit Date(int64_t microSec) : microSecondsSinceEpoch_(microSec){};
-    Date(unsigned int year,
-         unsigned int month,
-         unsigned int day,
-         unsigned int hour = 0,
-         unsigned int minute = 0,
-         unsigned int second = 0,
-         unsigned int microSecond = 0);
 
   private:
     int64_t microSecondsSinceEpoch_ = 0;
