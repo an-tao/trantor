e384af5a8e716f8ded4b23d81e7551a3cabe6461
Bertrand Darbon <bertrand.darbon@pollen-metrology.com>
2021-03-13 18:01:53 +0100

    Export Fmt explicit instantiations + ignore warning as public options

--- trantor/utils/LogStream.cc
+++ trantor/utils/LogStream.cc
@@ -258,16 +258,16 @@ Fmt::Fmt(const char *fmt, T val)
 
 // Explicit instantiations
 
-template Fmt::Fmt(const char *fmt, char);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, char);
 
-template Fmt::Fmt(const char *fmt, short);
-template Fmt::Fmt(const char *fmt, unsigned short);
-template Fmt::Fmt(const char *fmt, int);
-template Fmt::Fmt(const char *fmt, unsigned int);
-template Fmt::Fmt(const char *fmt, long);
-template Fmt::Fmt(const char *fmt, unsigned long);
-template Fmt::Fmt(const char *fmt, long long);
-template Fmt::Fmt(const char *fmt, unsigned long long);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, short);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, unsigned short);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, int);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, unsigned int);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, long);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, unsigned long);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, long long);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, unsigned long long);
 
-template Fmt::Fmt(const char *fmt, float);
-template Fmt::Fmt(const char *fmt, double);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, float);
+template TRANTOR_EXPORT Fmt::Fmt(const char *fmt, double);
