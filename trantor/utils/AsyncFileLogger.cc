0923f6d926075a707c80fb13d2a7d9e808ec2f07
Martin Chang <marty1885@users.noreply.github.com>
2021-07-26 23:29:04 +0800

    Detect and handle MinGW (#161)

--- trantor/utils/AsyncFileLogger.cc
+++ trantor/utils/AsyncFileLogger.cc
@@ -237,14 +237,14 @@ AsyncFileLogger::LoggerFile::~LoggerFile()
             filePath_ + fileBaseName_ + "." +
             creationDate_.toCustomedFormattedString("%y%m%d-%H%M%S") +
             std::string(seq) + fileExtName_;
-#ifndef _WIN32
+#if !defined(_WIN32) || defined(__MINGW32__)
         rename(fileFullName_.c_str(), newName.c_str());
-#else   // _WIN32
+#else
         // Convert UTF-8 file to UCS-2
         auto wFullName{utils::toNativePath(fileFullName_)};
         auto wNewName{utils::toNativePath(newName)};
         _wrename(wFullName.c_str(), wNewName.c_str());
-#endif  // _WIN32
+#endif
     }
 }
 
