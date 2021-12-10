4b66320d34eb4c11010eddb75b281ada4618a5f9
An Tao <antao2002@gmail.com>
2021-12-10 11:56:15 +0800

    Fix the error when sending partial files (#192)

--- trantor/net/inner/TcpConnectionImpl.cc
+++ trantor/net/inner/TcpConnectionImpl.cc
@@ -1513,7 +1513,9 @@ void TcpConnectionImpl::sendFileInLoop(const BufferNodePtr &filePtr)
     {
         auto n = read(filePtr->sendFd_,
                       &(*fileBufferPtr_)[0],
-                      fileBufferPtr_->size());
+                      std::min(fileBufferPtr_->size(),
+                               static_cast<decltype(fileBufferPtr_->size())>(
+                                   filePtr->fileBytesToSend_)));
 #else
     _fseeki64(filePtr->sendFp_, filePtr->offset_, SEEK_SET);
     if (!fileBufferPtr_)
@@ -1522,9 +1524,12 @@ void TcpConnectionImpl::sendFileInLoop(const BufferNodePtr &filePtr)
     }
     while (filePtr->fileBytesToSend_ > 0)
     {
+        auto bytes = static_cast<decltype(fileBufferPtr_->size())>(
+            filePtr->fileBytesToSend_);
         auto n = fread(&(*fileBufferPtr_)[0],
                        1,
-                       fileBufferPtr_->size(),
+                       (fileBufferPtr_->size() < bytes ? fileBufferPtr_->size()
+                                                       : bytes),
                        filePtr->sendFp_);
 #endif
         if (n > 0)
