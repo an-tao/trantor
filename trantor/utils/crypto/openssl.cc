708363877b8ae7dfcffc2db89b11a2b1ea955d59
marty1885 <marty188586@gmail.com>
2023-02-09 21:58:21 +0800

    Fix OpenSSL 1.x compat

--- trantor/utils/crypto/openssl.cc
+++ trantor/utils/crypto/openssl.cc
@@ -9,6 +9,7 @@ namespace utils
 
 Hash128 md5(const void* data, size_t len)
 {
+#if OPENSSL_VERSION_MAJOR >= 3
     Hash128 hash;
     auto md5 = EVP_MD_fetch(nullptr, "MD5", nullptr);
     auto ctx = EVP_MD_CTX_new();
@@ -18,10 +19,19 @@ Hash128 md5(const void* data, size_t len)
     EVP_MD_CTX_free(ctx);
     EVP_MD_free(md5);
     return hash;
+#else
+    Hash128 hash;
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, data, len);
+    MD5_Final((unsigned char*)&hash, &ctx);
+    return hash;
+#endif
 }
 
 Hash160 sha1(const void* data, size_t len)
 {
+#if OPENSSL_VERSION_MAJOR >= 3
     Hash160 hash;
     auto sha1 = EVP_MD_fetch(nullptr, "SHA1", nullptr);
     auto ctx = EVP_MD_CTX_new();
@@ -31,10 +41,19 @@ Hash160 sha1(const void* data, size_t len)
     EVP_MD_CTX_free(ctx);
     EVP_MD_free(sha1);
     return hash;
+#else
+    Hash160 hash;
+    SHA_CTX ctx;
+    SHA1_Init(&ctx);
+    SHA1_Update(&ctx, data, len);
+    SHA1_Final((unsigned char*)&hash, &ctx);
+    return hash;
+#endif
 }
 
 Hash256 sha256(const void* data, size_t len)
 {
+#if OPENSSL_VERSION_MAJOR >= 3
     Hash256 hash;
     auto sha256 = EVP_MD_fetch(nullptr, "SHA256", nullptr);
     auto ctx = EVP_MD_CTX_new();
@@ -44,10 +63,19 @@ Hash256 sha256(const void* data, size_t len)
     EVP_MD_CTX_free(ctx);
     EVP_MD_free(sha256);
     return hash;
+#else
+    Hash256 hash;
+    SHA256_CTX ctx;
+    SHA256_Init(&ctx);
+    SHA256_Update(&ctx, data, len);
+    SHA256_Final((unsigned char*)&hash, &ctx);
+    return hash;
+#endif
 }
 
 Hash256 sha3(const void* data, size_t len)
 {
+#if OPENSSL_VERSION_MAJOR >= 3
     Hash256 hash;
     auto sha3 = EVP_MD_fetch(nullptr, "SHA3-256", nullptr);
     auto ctx = EVP_MD_CTX_new();
@@ -57,6 +85,16 @@ Hash256 sha3(const void* data, size_t len)
     EVP_MD_CTX_free(ctx);
     EVP_MD_free(sha3);
     return hash;
+#else
+    Hash256 hash;
+    auto sha3 = EVP_sha3_256();
+    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, sha3, nullptr);
+    EVP_DigestUpdate(ctx, data, len);
+    EVP_DigestFinal_ex(ctx, (unsigned char*)&hash, nullptr);
+    EVP_MD_CTX_free(ctx);
+    return hash;
+#endif
 }
 
 }  // namespace utils
