10b4d0ef6ba12e822e0a7145e091309c90f5b658
marty1885 <marty188586@gmail.com>
2023-04-28 01:19:55 +0800

    fix OpenSSL 1.3 build

--- trantor/net/inner/tlsprovider/OpenSSLProvider.cc
+++ trantor/net/inner/tlsprovider/OpenSSLProvider.cc
@@ -19,10 +19,13 @@ using namespace trantor;
 
 // Force OpenSSL to initialize before main() is called
 static bool sslInitFlag = []() {
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
+    OPENSSL_init_ssl(0, nullptr);
+    OPENSSL_init_crypto(0, nullptr);
+#else
     SSL_library_init();
     OpenSSL_add_all_algorithms();
     SSL_load_error_strings();
-#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
     ERR_load_BIO_strings();
     ERR_load_crypto_strings();
 #endif
