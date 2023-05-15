88a22297ee45555fb2d79e4dbcae198aef4825f2
Mis1eader-dev <muhammad.luqman.faiq@gmail.com>
2023-05-15 07:07:16 +0300

    Added test case for `toIpPortNetEndian`

--- trantor/unittests/InetAddressUnittest.cc
+++ trantor/unittests/InetAddressUnittest.cc
@@ -15,6 +15,33 @@ TEST(InetAddress, innerIpTest)
     EXPECT_EQ(false, InetAddress("127.0.0.2", 0).isUnspecified());
     EXPECT_EQ(false, InetAddress("0.0.0.0", 0).isUnspecified());
 }
+TEST(InetAddress, toIpPortNetEndianTest)
+{
+    EXPECT_EQ(
+        std::string(
+            {char(192), char(168), 0, 1, (80 & 0xff00) >> 8, 80 & 0x00ff}),
+        InetAddress("192.168.0.1", 80).toIpPortNetEndian());
+    EXPECT_EQ(std::string({0x20,
+                           0x01,
+                           0x0d,
+                           char(0xb8),
+                           0x33,
+                           0x33,
+                           0x44,
+                           0x44,
+                           0x55,
+                           0x55,
+                           0x66,
+                           0x66,
+                           0x77,
+                           0x77,
+                           char(0x88),
+                           char(0x88),
+                           char((443 & 0xff00) >> 8),
+                           char(443 & 0x00ff)}),
+              InetAddress("2001:0db8:3333:4444:5555:6666:7777:8888", 443, true)
+                  .toIpPortNetEndian());
+}
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
