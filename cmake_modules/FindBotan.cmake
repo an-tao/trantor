1f550ea0016cb540a11b4006d2aa12582cda656d
marty1885 <marty188586@gmail.com>
2023-06-12 22:40:05 +0800

    Support finding botan3

--- cmake_modules/FindBotan.cmake
+++ cmake_modules/FindBotan.cmake
@@ -1,21 +1,24 @@
+function(find_botan_pkgconfig package_name)
+  if (TARGET Botan::Botan)
+    return()
+  endif ()
 
-find_package(PkgConfig)
-if(NOT WIN32 AND PKG_CONFIG_FOUND)
-  if (NOT TARGET Botan::Botan)
-    pkg_check_modules(Botan QUIET IMPORTED_TARGET botan-2)
-    if (TARGET PkgConfig::Botan)
-      add_library(Botan::Botan ALIAS PkgConfig::Botan)
-    endif ()
-  endif()
-endif()
+  pkg_check_modules(Botan QUIET IMPORTED_TARGET ${package_name})
+  if (TARGET PkgConfig::Botan)
+    add_library(Botan::Botan ALIAS PkgConfig::Botan)
+  endif ()
+endfunction()
 
-if (NOT TARGET Botan::Botan)
+function(find_botan_search package_name)
+  if (TARGET Botan::Botan)
+    return()
+  endif ()
   find_path(Botan_INCLUDE_DIRS NAMES botan/botan.h
-            PATH_SUFFIXES botan-2
+            PATH_SUFFIXES ${package_name}
             DOC "The Botan include directory")
 
-  find_library(Botan_LIBRARIES NAMES botan botan-2
-               DOC "The Botan library")
+  find_library(Botan_LIBRARIES NAMES botan ${package_name}
+              DOC "The Botan library")
 
   mark_as_advanced(Botan_INCLUDE_DIRS Botan_LIBRARIES)
 
@@ -30,7 +33,19 @@ if (NOT TARGET Botan::Botan)
   if (WIN32)
     target_compile_definitions(Botan::Botan INTERFACE -DNOMINMAX=1)
   endif ()
-endif ()
+endfunction()
+
+
+find_package(PkgConfig)
+if(NOT WIN32 AND PKG_CONFIG_FOUND)
+  find_botan_pkgconfig(botan-2)
+  find_botan_pkgconfig(botan-3)
+endif()
+
+if(NOT TARGET Botan::Botan)
+  find_botan_search(botan-2)
+  find_botan_search(botan-3)
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
