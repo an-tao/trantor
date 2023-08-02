aee8856cc0e38beb73a605b3e464cfca2ea50aa3
Martin Chang <marty1885@users.noreply.github.com>
2023-08-02 15:53:05 +0000

    add fallback when OpenSSL not providing BLAKE2b (#275)

--- trantor/utils/crypto/openssl.cc
+++ trantor/utils/crypto/openssl.cc
@@ -7,16 +7,13 @@
 #include <openssl/sha.h>
 #endif
 
-// Hack: LibreSSL does not support SHA3. We use our own implementation.
-#if defined(LIBRESSL_VERSION_NUMBER) || OPENSSL_VERSION_NUMBER < 0x10100000L
 #include "sha3.h"
 #include "sha3.cc"
-#endif
 
-#if OPENSSL_VERSION_MAJOR < 3 || defined(LIBRESSL_VERSION_NUMBER)
+// Some OpenSSL installations does not come with BLAKE2b-256
+// We use our own implementation in such case
 #include "blake2.h"
 #include "blake2.cc"
-#endif
 
 namespace trantor
 {
@@ -90,49 +87,54 @@ Hash256 sha256(const void* data, size_t len)
 
 Hash256 sha3(const void* data, size_t len)
 {
-#if defined(LIBRESSL_VERSION_NUMBER) || (OPENSSL_VERSION_NUMBER < 0x10100000L)
-    Hash256 hash;
-    trantor_sha3((const unsigned char*)data, len, &hash, sizeof(hash));
-    return hash;
-#elif OPENSSL_VERSION_MAJOR >= 3
+#if OPENSSL_VERSION_MAJOR >= 3
     Hash256 hash;
     auto sha3 = EVP_MD_fetch(nullptr, "SHA3-256", nullptr);
-    auto ctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(ctx, sha3, nullptr);
-    EVP_DigestUpdate(ctx, data, len);
-    EVP_DigestFinal_ex(ctx, (unsigned char*)&hash, nullptr);
-    EVP_MD_CTX_free(ctx);
-    EVP_MD_free(sha3);
-    return hash;
-#else
+    if (sha3 != nullptr)
+    {
+        auto ctx = EVP_MD_CTX_new();
+        EVP_DigestInit_ex(ctx, sha3, nullptr);
+        EVP_DigestUpdate(ctx, data, len);
+        EVP_DigestFinal_ex(ctx, (unsigned char*)&hash, nullptr);
+        EVP_MD_CTX_free(ctx);
+        EVP_MD_free(sha3);
+        return hash;
+    }
+#elif !defined(LIBRESSL_VERSION_NUMBER)
     Hash256 hash;
     auto sha3 = EVP_sha3_256();
-    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(ctx, sha3, nullptr);
-    EVP_DigestUpdate(ctx, data, len);
-    EVP_DigestFinal_ex(ctx, (unsigned char*)&hash, nullptr);
-    EVP_MD_CTX_free(ctx);
-    return hash;
+    if (sha3 != nullptr)
+    {
+        EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+        EVP_DigestInit_ex(ctx, sha3, nullptr);
+        EVP_DigestUpdate(ctx, data, len);
+        EVP_DigestFinal_ex(ctx, (unsigned char*)&hash, nullptr);
+        EVP_MD_CTX_free(ctx);
+        return hash;
+    }
 #endif
+    trantor_sha3((const unsigned char*)data, len, &hash, sizeof(hash));
+    return hash;
 }
 
 Hash256 blake2b(const void* data, size_t len)
 {
+    Hash256 hash;
 #if OPENSSL_VERSION_MAJOR >= 3
-    Hash256 hash;
     auto blake2b = EVP_MD_fetch(nullptr, "BLAKE2b-256", nullptr);
-    auto ctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(ctx, blake2b, nullptr);
-    EVP_DigestUpdate(ctx, data, len);
-    EVP_DigestFinal_ex(ctx, (unsigned char*)&hash, nullptr);
-    EVP_MD_CTX_free(ctx);
-    EVP_MD_free(blake2b);
-    return hash;
-#else
-    Hash256 hash;
+    if (blake2b != nullptr)
+    {
+        auto ctx = EVP_MD_CTX_new();
+        EVP_DigestInit_ex(ctx, blake2b, nullptr);
+        EVP_DigestUpdate(ctx, data, len);
+        EVP_DigestFinal_ex(ctx, (unsigned char*)&hash, nullptr);
+        EVP_MD_CTX_free(ctx);
+        EVP_MD_free(blake2b);
+        return hash;
+    }
+#endif
     trantor_blake2b(&hash, sizeof(hash), data, len, nullptr, 0);
     return hash;
-#endif
 }
 
 }  // namespace utils
