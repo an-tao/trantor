a3a5763d6c93b6493f864a0c9967a5828198dd78
Robert Huang <56649783@qq.com>
2023-12-10 17:22:39 +0800

    add cmake-format

--- format.sh
+++ format.sh
@@ -4,3 +4,6 @@ clang-format --version
 
 find trantor -name *.h -o -name *.cc -exec dos2unix {} \;
 find trantor -name *.h -o -name *.cc|xargs clang-format -i -style=file
+
+cmake-format --version
+find . -name CMakeLists.txt|xargs cmake-format -i
