fc2a7041cda6f06325eb557d708534a7e8487f91
Martin Chang <marty1885@users.noreply.github.com>
2023-12-16 00:52:40 +0800

    use fallback hashing when botan does not support it

--- trantor/utils/crypto/botan.cc
+++ trantor/utils/crypto/botan.cc
@@ -3,6 +3,11 @@
 
 #include <cassert>
 
+#include "md5.h"
+#include "sha1.h"
+#include "sha3.h"
+#include "blake2.h"
+
 namespace trantor
 {
 namespace utils
@@ -11,6 +16,15 @@ Hash128 md5(const void* data, size_t len)
 {
     Hash128 hash;
     auto md5 = Botan::HashFunction::create("MD5");
+    if (md5 == nullptr)
+    {
+        MD5_CTX ctx;
+        trantor_md5_init(&ctx);
+        trantor_md5_update(&ctx, (const unsigned char*)data, len);
+        trantor_md5_final(&ctx, (unsigned char*)&hash);
+        return hash;
+    }
+
     md5->update((const unsigned char*)data, len);
     md5->final((unsigned char*)&hash);
     return hash;
@@ -20,6 +34,14 @@ Hash160 sha1(const void* data, size_t len)
 {
     Hash160 hash;
     auto sha1 = Botan::HashFunction::create("SHA-1");
+    if (sha1 == nullptr)
+    {
+        SHA1_CTX ctx;
+        TrantorSHA1Init(&ctx);
+        TrantorSHA1Update(&ctx, (const unsigned char*)data, len);
+        TrantorSHA1Final((unsigned char*)&hash, &ctx);
+        return hash;
+    }
     sha1->update((const unsigned char*)data, len);
     sha1->final((unsigned char*)&hash);
     return hash;
@@ -29,6 +51,7 @@ Hash256 sha256(const void* data, size_t len)
 {
     Hash256 hash;
     auto sha256 = Botan::HashFunction::create("SHA-256");
+    assert(sha256 != nullptr);  // Botan guarantees that SHA-256 is available
     sha256->update((const unsigned char*)data, len);
     sha256->final((unsigned char*)&hash);
     return hash;
@@ -38,6 +61,11 @@ Hash256 sha3(const void* data, size_t len)
 {
     Hash256 hash;
     auto sha3 = Botan::HashFunction::create("SHA-3(256)");
+    if (sha3 == nullptr)
+    {
+        trantor_sha3((const unsigned char*)data, len, &hash, sizeof(hash));
+        return hash;
+    }
     assert(sha3 != nullptr);
     sha3->update((const unsigned char*)data, len);
     sha3->final((unsigned char*)&hash);
@@ -48,7 +76,11 @@ Hash256 blake2b(const void* data, size_t len)
 {
     Hash256 hash;
     auto blake2b = Botan::HashFunction::create("BLAKE2b(256)");
-    assert(blake2b != nullptr);
+    if (blake2b == nullptr)
+    {
+        trantor_blake2b(&hash, sizeof(hash), data, len, NULL, 0);
+        return hash;
+    }
     blake2b->update((const unsigned char*)data, len);
     blake2b->final((unsigned char*)&hash);
     return hash;
