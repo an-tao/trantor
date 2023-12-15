354803ae079bfd1c094a4b2559c07932e506a4cf
Martin Chang <marty1885@users.noreply.github.com>
2023-12-16 01:09:36 +0800

    cleanup botan codebase

--- trantor/utils/crypto/botan.cc
+++ trantor/utils/crypto/botan.cc
@@ -2,11 +2,27 @@
 #include <trantor/utils/Utilities.h>
 
 #include <cassert>
+#include <string_view>
 
 #include "md5.h"
 #include "sha1.h"
 #include "sha3.h"
 #include "blake2.h"
+
+template <typename Hash>
+inline bool attemptHash(const std::string_view& name,
+                        Hash& hash,
+                        const void* data,
+                        size_t len)
+{
+    auto hashFunction = Botan::HashFunction::create(name);
+    if (hashFunction == nullptr)
+        return false;
+
+    hashFunction->update((const unsigned char*)data, len);
+    hashFunction->final((unsigned char*)&hash);
+    return true;
+}
 
 namespace trantor
 {
@@ -15,74 +31,57 @@ namespace utils
 Hash128 md5(const void* data, size_t len)
 {
     Hash128 hash;
-    auto md5 = Botan::HashFunction::create("MD5");
-    if (md5 == nullptr)
-    {
-        MD5_CTX ctx;
-        trantor_md5_init(&ctx);
-        trantor_md5_update(&ctx, (const unsigned char*)data, len);
-        trantor_md5_final(&ctx, (unsigned char*)&hash);
+    if (attemptHash("MD5", hash, data, len))
         return hash;
-    }
 
-    md5->update((const unsigned char*)data, len);
-    md5->final((unsigned char*)&hash);
+    MD5_CTX ctx;
+    trantor_md5_init(&ctx);
+    trantor_md5_update(&ctx, (const unsigned char*)data, len);
+    trantor_md5_final(&ctx, (unsigned char*)&hash);
     return hash;
 }
 
 Hash160 sha1(const void* data, size_t len)
 {
     Hash160 hash;
-    auto sha1 = Botan::HashFunction::create("SHA-1");
-    if (sha1 == nullptr)
-    {
-        SHA1_CTX ctx;
-        TrantorSHA1Init(&ctx);
-        TrantorSHA1Update(&ctx, (const unsigned char*)data, len);
-        TrantorSHA1Final((unsigned char*)&hash, &ctx);
+    if (attemptHash("SHA-1", hash, data, len))
         return hash;
-    }
-    sha1->update((const unsigned char*)data, len);
-    sha1->final((unsigned char*)&hash);
+
+    SHA1_CTX ctx;
+    TrantorSHA1Init(&ctx);
+    TrantorSHA1Update(&ctx, (const unsigned char*)data, len);
+    TrantorSHA1Final((unsigned char*)&hash, &ctx);
     return hash;
 }
 
 Hash256 sha256(const void* data, size_t len)
 {
     Hash256 hash;
-    auto sha256 = Botan::HashFunction::create("SHA-256");
-    assert(sha256 != nullptr);  // Botan guarantees that SHA-256 is available
-    sha256->update((const unsigned char*)data, len);
-    sha256->final((unsigned char*)&hash);
+    // Botan should guarantees that SHA-256 is available
+    bool ok = attemptHash("SHA-256", hash, data, len);
+    if (!ok)
+        assert(false);
     return hash;
 }
 
 Hash256 sha3(const void* data, size_t len)
 {
     Hash256 hash;
-    auto sha3 = Botan::HashFunction::create("SHA-3(256)");
-    if (sha3 == nullptr)
-    {
-        trantor_sha3((const unsigned char*)data, len, &hash, sizeof(hash));
+    if (attemptHash("SHA-3(256)", hash, data, len))
         return hash;
-    }
-    assert(sha3 != nullptr);
-    sha3->update((const unsigned char*)data, len);
-    sha3->final((unsigned char*)&hash);
+
+    trantor_sha3((const unsigned char*)data, len, &hash, sizeof(hash));
     return hash;
 }
 
 Hash256 blake2b(const void* data, size_t len)
 {
     Hash256 hash;
-    auto blake2b = Botan::HashFunction::create("BLAKE2b(256)");
-    if (blake2b == nullptr)
-    {
-        trantor_blake2b(&hash, sizeof(hash), data, len, NULL, 0);
+    if (attemptHash("BLAKE2b(256)", hash, data, len))
         return hash;
-    }
-    blake2b->update((const unsigned char*)data, len);
-    blake2b->final((unsigned char*)&hash);
+    auto blake2b = Botan::HashFunction::create("BLAKE2b(256)");
+
+    trantor_blake2b(&hash, sizeof(hash), data, len, NULL, 0);
     return hash;
 }
 
