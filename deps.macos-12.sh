84cd5779ba32406e9babb36c02d391fd4a6c0856
Robert Huang <56649783@qq.com>
2024-03-14 21:43:38 +0800

    fix brew install version bugs

--- deps.macos-12.sh
+++ deps.macos-12.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # This script is used to install dependencies on MacOS 12.0 or newer
-brew update
-# TODO: botan v3, botan@3.3.0 build failed, if newer version work, remove @3.2.0
-brew install googletest spdlog c-ares openssl@3  botan@3.2.0
+# TODO: botan v3, botan@3.3.0 build failed, if newer version work, turn on brew update
+# brew update
+brew install googletest spdlog c-ares openssl@3  botan
