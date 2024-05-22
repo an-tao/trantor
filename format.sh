621bcbeb2d37d357cd4ef7e77e4b16e81bea5db5
Robert Huang <56649783@qq.com>
2024-05-22 12:53:59 +0800

    combine clang-format and cmake-format exit code

--- format.sh
+++ format.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 clang-format --version
+echo "cmake-format version: $(cmake-format --version)"
 
 find trantor -name *.h -o -name *.cc -exec dos2unix {} \;
-find trantor -name *.h -o -name *.cc|xargs clang-format -i -style=file
 
-cmake-format --version
-find . -maxdepth 1 -name CMakeLists.txt|xargs cmake-format -i
-find trantor -name CMakeLists.txt|xargs cmake-format -i
-find cmake -name *.cmake -o -name *.cmake.in|xargs cmake-format -i
-find cmake_modules -name *.cmake -o -name *.cmake.in|xargs cmake-format -i
+find trantor -name *.h -o -name *.cc|xargs clang-format -i -style=file \
+&& find . -maxdepth 1 -name CMakeLists.txt|xargs cmake-format -i \
+&& find trantor -name CMakeLists.txt|xargs cmake-format -i \
+&& find cmake -name *.cmake -o -name *.cmake.in|xargs cmake-format -i \
+&& find cmake_modules -name *.cmake -o -name *.cmake.in|xargs cmake-format -i
