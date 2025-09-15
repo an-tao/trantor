dd365c7860d85720e8874235c9552ec4c9e9624c
naoto <naotofujiwara890@gmail.com>
2025-09-15 16:58:39 +0900

    use static constexpr

--- trantor/utils/Date.h
+++ trantor/utils/Date.h
@@ -18,8 +18,6 @@
 #include <stdint.h>
 #include <string>
 
-#define MICRO_SECONDS_PER_SEC 1000000LL
-
 namespace trantor
 {
 /**
@@ -346,6 +344,8 @@ class TRANTOR_EXPORT Date
     {
         std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
     }
+
+    static constexpr long MICRO_SECONDS_PER_SEC = 1000000LL;
 
   private:
     int64_t microSecondsSinceEpoch_{0};
